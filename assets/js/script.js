99250e6d9070c0bd1e8b500c2a18e2dea84272ae
unknown <russelhkamal@gmail.com>
2022-08-02 23:08:40 -0400

    finished method to fetch data from query

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,10 @@ let weeklyTitle = $("#5-day-title");
 var date = moment().format('dddd, MMMM Do YYYY');
 var cityHist = [];
 
+function init() {
+    searchForm.children("button").on("click", getData)
+}
+
 function storeCities() {
     if(localStorage.getItem("cityHist") !== null) {
         cityHist = JSON.parse(localStorage.getItem("cityHist"));
@@ -25,6 +29,53 @@ function printCities() {
     }
     searchHistory.children("button").on("click", getData);
 }
+
+function getData(e) {
+    e.preventDefault();
+    let city = "";
+    if(e.target.textContent === "Search") {
+        city = searchForm.children("input").val();
+        //clears search bar after a search
+        searchForm.children("input").val("");
+    }
+    else {
+        city = e.target.textContent;
+    }
+    city = city.toUpperCase();
+
+    let requestURL = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}&units=metric`;
+    fetch(requestURL)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (data) {
+        if(data.length) {
+            let lattitude = data[0].lat;
+            let longitude = data[0].lon;
+            requestURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=metric`;
+            fetch(requestURL)
+            .then (function (res) {
+                return res.json();
+            })
+            .then(function(data) {
+                displayWeather(data, city);
+                displayForecast(data);
+                saveCity(city);
+            });
+        }
+    });
+}
+
+function displayWeather(data, city) {
+    let title = currentWeather.children().eq(0).children("h2");
+    let weatherImage = currentWeather.children().eq(0).children("img");
+    let temp = currentWeather.children().eq(1);
+    let wind = currentWeather.children().eq(2);
+    let humidity = currentWeather.children().eq(3);
+    let uV = currentWeather.children().eq(4);
+    currentWeather.addClass("card bg-secondary")
+}
+
 
 
 
